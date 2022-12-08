87c4c9f4b2a4ca47b2bbc9999fdd213833f5ca17
Max <maxim.dominus@gmail.com>
2022-12-09 00:52:24 +0300

    задача спринта 3 на ревью

--- Counter Max/ViewController.swift
+++ Counter Max/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
+private var countOneTouch = 0
+private let textMessage = "Значение счётчика: "
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textCounter: UILabel!
+     
+    @IBAction func plusOne(_ sender: Any) {
+    
+        countOneTouch += 1
+                   
+        textCounter.text = "\(textMessage)\(countOneTouch)"
+    }
+    
     override func viewDidLoad() {
+                   
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textCounter.text = "Значение счётчика: 0"
+        
+        
+        
     }
 
 
